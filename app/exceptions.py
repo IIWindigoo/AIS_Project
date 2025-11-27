99e83ff9a14183c02a036eda5dbff41d2b7db09f
Ruslan B <rusbez04@yandex.ru>
2025-11-28 02:07:53 +0500

    +исключения

--- app/exceptions.py
+++ app/exceptions.py
@@ -69,3 +69,8 @@ RequestBadStatus = HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
 MembershipIsActive = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
                                    detail="У вас уже есть действующий абонемент. Новая заявка будет доступна после его окончания.")
 
+ActiveMembershipNofFound = HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                                         detail="Активный абонемент не найден")
+
+OnlyForClient = HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+                              detail="Доступ только для клиентов")
